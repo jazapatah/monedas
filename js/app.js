204d9149bc9a69cbc45ca9b70a77315496b1c819
Jonatan <jotan.alexander@gmail.com>
2023-07-25 18:55:31 -0500

    starts app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Declarar variables para capturar la información
+let rate1 = document.querySelector('.rate1');
+let rate2 = document.querySelector('.rate2');
+let resultBtn = document.querySelector('.result');
+let selects = document.querySelectorAll('.options select');
+let sel1 = selects[0];
+let sel2 = selects[1];
+let inputs = document.querySelectorAll('.input input');
