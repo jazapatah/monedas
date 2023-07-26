0d6965f2a866efd52987e45c120375ca409f544f
Jonatan <jotan.alexander@gmail.com>
2023-07-25 19:28:17 -0500

    continue app.js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,35 @@ let selects = document.querySelectorAll('.options select');
 let sel1 = selects[0];
 let sel2 = selects[1];
 let inputs = document.querySelectorAll('.input input');
+let inpt1 = inputs[0];
+let inpt2 = inputs[1];
+
+let rates = {};
+let requestUrl = "https://api.exchangerate.host/latest?base=USD";
+
+// Funcion async
+/**
+ * await dice que hay que esperar que toma la respuesta de la api requestUrl
+ */
+async function fetchRates(){
+    let res = await fetch(requestUrl);
+    rer = await res.json();
+    rates = res.rates();
+}
+
+// Función tradicional
+/**
+ * Función para las opciones
+ */
+
+function populateOptions() {
+    let val = '';
+    Object.keys(rates).forEach(code => {
+        let str = `<option value="${code}">${code}</option>`;
+        // incrementar el option
+        // val += str; equivalente a val = val +str;
+        val += str;
+    })
+    // mostrar las opciones
+    selects.forEach((s)=>(s.innerHTML = val));
+}
